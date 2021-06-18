a394966ba46a2ae31ef8b169229a441c25bf418e
CCXXXI <ccxxxi@qq.com>
2021-06-19 01:44:36 +0800

    reformat doc

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='将自动化规则应用到全站条目，循环执行。',
         epilog='\n\n'.join(f'{i}\n{job}' for i, job in enumerate(jobs_list)),
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     parser.add_argument(
         'start',
-        help='从第几个任务开始执行',
+        help='从第几个任务开始执行 (default: 0)',
         type=int,
         nargs='?',
         default=0,
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '-s',
         '--simulate',
-        help='不对服务器内容做任何实际更改，只显示将更改的内容',
+        help='不对服务器内容做任何实际更改，只显示将更改的内容 (default: False)',
         action='store_true',
     )
     args = parser.parse_args()
