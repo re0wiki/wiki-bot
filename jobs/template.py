00b0d4d8d91274f9545c45835f67770c9c5f8e2f
CCXXXI <ccxxxi@qq.com>
2021-06-19 02:03:00 +0800

    Create template.py

---
+++ jobs/template.py
@@ -0,0 +1,26 @@
+from ._jobs import CmdJob, add_job
+
+# noinspection SpellCheckingInspection
+pairs = [
+    ('Character', 'Infobox character'),
+    ('Re:Zero Light Novel Volumes', 'Infobox novel'),
+    ('Parent Tab', None),
+    ('Lugnica Navbox', None),
+    ('Vollachia Navbox', None),
+    ('Royal Election Navbox', None),
+    ('Anime Navigation', None),
+    ('Manga Navigation', None),
+    ('Disambig', None),
+]
+
+repl = ['template', '-always']
+rm = repl + ['-remove']
+
+for o, n in pairs:
+    if n:
+        repl += [o, n]
+    else:
+        rm.append(o)
+
+add_job(CmdJob(repl))
+add_job(CmdJob(rm))
