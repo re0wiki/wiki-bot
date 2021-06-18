abe9dec2f25e0fe1b67464becd724efc1acbf02c
CCXXXI <ccxxxi@qq.com>
2021-06-19 01:57:49 +0800

    Create _starts.py

---
+++ jobs/_starts.py
@@ -0,0 +1,19 @@
+def ns2start(ns: str):
+    return f'-start:"{ns}:!"'
+
+
+def nss2starts(nss):
+    return [ns2start(ns) for ns in nss]
+
+
+ns_base = ['', 'project', 'template', 'category', 'file']
+ns_more = ns_base + ['module', 'mediawiki']
+# unused
+# _ns_full = ns_more + ['user', 'help']
+
+starts_base = nss2starts(ns_base)
+starts_more = nss2starts(ns_more)
+
+if __name__ == '__main__':
+    print(starts_base)
+    print(starts_more)
