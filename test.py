550fa5f8af7387d2ff57101bcde24b888c9e85f8
CCXXXI <ccxxxi@qq.com>
2021-06-21 17:05:16 +0800

    move unittest to separate file (#11)
    * move unittest to separate file
    
    * add doc

---
+++ test.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestJobs(unittest.TestCase):
+    """自动化规则"""
+
+    def test_repl_name(self):
+        """similar_chars不应重复"""
+        from jobs.repl.name import similar_chars
+
+        all_chars = "".join(similar_chars)
+        self.assertEqual(len(all_chars), len(set(all_chars)))
+
+
+if __name__ == "__main__":
+    unittest.main()
