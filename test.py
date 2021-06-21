4e4cff19c3529ac0739fed3ed334665a3137d575
CCXXXI <ccxxxi@qq.com>
2021-06-21 18:45:04 +0800

    update doc

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import unittest
 
 
 class TestJobs(unittest.TestCase):
-    """自动化规则"""
+    """测试自动化规则。"""
 
     def test_repl_name(self):
-        """similar_chars不应重复"""
+        """similar_chars不应重复。"""
         from jobs.repl.name import similar_chars
 
         all_chars = "".join(similar_chars)
