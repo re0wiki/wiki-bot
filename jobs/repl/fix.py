08f2a0ce8d2ec7d5d40fe7fc86d56cbd8e2599f4
CCXXXI <ccxxxi@qq.com>
2021-07-20 04:48:25 +0800

    fix.py: 移除多余命令行参数

--- jobs/repl/fix.py
+++ jobs/repl/fix.py
@@ -1,5 +1,4 @@
 from ..jobs_ import CmdJob, add_job
-from ..starts_ import starts_base
 
 add_job(
     CmdJob(
@@ -7,12 +6,10 @@ add_job(
             "replace",
             "-automaticsummary",
             "-always",
-            "-nocase",
             "-fix:HTML",
             "-fix:syntax",
             "-fix:isbn",
             "-fix:specialpages",
         ]
-        + starts_base
     )
 )
