3522ab689b53865df98accb82772288e32278601
CCXXXI <ccxxxi@qq.com>
2021-07-21 11:09:06 +0800

    random touch

--- jobs/redirect.py
+++ jobs/redirect.py
@@ -1,6 +1,6 @@
 from .jobs_ import CmdJob, add_job
 
-base = ["redirect", "-always", "-randomredirect", "-limit:128"]
+base = ["redirect", "-always", "-randomredirect:128"]
 
 add_job(CmdJob(base + ["do"]))
 add_job(CmdJob(base + ["br"]))
