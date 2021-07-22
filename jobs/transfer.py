aa77000ca484b0a93c8995c4b8c3042c15e0af82
CCXXXI <ccxxxi@qq.com>
2021-07-23 02:24:15 +0800

    transfer Gadget.*css from zhcc (#41)
    * Update pywikibot
    
    * Update pywikibot
    
    * transfer Gadget.*css from zhcc

--- jobs/transfer.py
+++ jobs/transfer.py
@@ -10,3 +10,18 @@ add_job(
         ]
     )
 )
+
+add_job(
+    CmdJob(
+        [
+            "transferbot",
+            "-family:cc",
+            "-lang:zh",
+            "-tofamily:re0",
+            "-tolang:zh",
+            "-start:mediawiki:!",
+            "-titleregex:Gadget.*css",
+            "-overwrite",
+        ]
+    )
+)
