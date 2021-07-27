ce1689f92ff91efa2a91a4d30a7629673da2c2ac
CCXXXI <ccxxxi@qq.com>
2021-07-27 11:54:01 +0800

    fix bug

--- jobs/redirect.py
+++ jobs/redirect.py
@@ -3,4 +3,4 @@ from .jobs_ import CmdJob, add_job
 base = ["redirect", "-always", "-randomredirect:128"]
 
 add_job(CmdJob(base + ["do"]))
-add_job(CmdJob(base + ["br", "delete"]))
+add_job(CmdJob(base + ["br", "-delete"]))
