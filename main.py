9a9af45d5d4b14e9e2a957244f2b26a6de29203f
CCXXXI <ccxxxi@qq.com>
2021-09-23 17:41:03 +0800

    style: add regions

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ import sentry_sdk
 
 from jobs import jobs, run
 
+# region sentry
 sentry_sdk.init(
     "https://b9865f81742941e1b658462ed983cfe7@o996799.ingest.sentry.io/5975280",
     traces_sample_rate=1.0,
 )
 sentry_sdk.set_user({"ip_address": "{{auto}}"})
+# endregion
+
+# region logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
+# endregion
 
+# region parser
 parser = argparse.ArgumentParser(
     description="执行自动化规则。",
     epilog=jobs.info,
@@ -33,6 +39,7 @@ parser.add_argument(
     help="不对服务器内容做任何实际更改，只显示将更改的内容",
     action="store_true",
 )
+# endregion
 
 if __name__ == "__main__":
     args = parser.parse_args()
