160b56d5fff52b2ac38b8ed0c7e28d7b0f3205de
CCXXXI <ccxxxi@qq.com>
2022-08-03 08:48:12 +0800

    refactor: refactor rename.py

--- rename.py
+++ rename.py
@@ -2,24 +2,38 @@ import argparse
 import logging
 import re
 
+import sentry_sdk
+
 from jobs.jobs_ import CmdJob
 from jobs.starts_ import ns2start, ns_more, starts_more
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    )
+# region sentry
+sentry_sdk.init(
+    "https://b9865f81742941e1b658462ed983cfe7@o996799.ingest.sentry.io/5975280",
+    traces_sample_rate=1.0,
+)
+sentry_sdk.set_user({"ip_address": "{{auto}}"})
+# endregion
 
-    parser = argparse.ArgumentParser(description="移动页面 & 替换文本")
-    parser.add_argument("old")
-    parser.add_argument("new")
-    args = parser.parse_args()
+# region logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+# endregion
 
-    old, new = args.old, args.new
+# region parser
+parser = argparse.ArgumentParser(description="移动页面 & 替换文本")
+parser.add_argument("old")
+parser.add_argument("new")
 
+
+# endregion
+
+
+def rename(old, new):
+    """Move pages and replace text."""
     o_pages = []
-
     for ns in ns_more + ["file"]:
         pages = (
             CmdJob(["listpages", "-format:3", f"-titleregex:{old}", ns2start(ns)])
@@ -54,3 +68,8 @@ if __name__ == "__main__":
         ]
         + starts_more
     ).run()
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    rename(args.old, args.new)
