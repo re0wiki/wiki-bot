7ba622932e142b1414d1aed513ae4998f6ee686c
CCXXXI <ccxxxi@qq.com>
2022-11-22 03:30:28 +0800

    feat: sync galleries

---
+++ jobs/re0_gallery.py
@@ -0,0 +1,3 @@
+from .jobs_ import CmdJob, add_job
+
+add_job(CmdJob(["re0_gallery"]))
---
+++ scripts/re0_gallery.py
@@ -0,0 +1,41 @@
+import logging
+
+import regex as re
+from pywikibot import Page
+from pywikibot.pagegenerators import AllpagesPageGenerator
+from tqdm import tqdm
+
+pattern = re.compile(r"<gallery[^>]*>.*?</gallery>", re.DOTALL)
+
+
+def sync_galleries():
+    """Replace zh galleries with en galleries."""
+    for zh_page in tqdm(AllpagesPageGenerator(includeredirects=False)):
+        zh_page: Page
+
+        for link in zh_page.iterlanglinks():
+            if link.site.code == "en":
+                en_text: str = Page(link).text
+                break
+        else:
+            logging.info(f"no en page for {zh_page.title()}")
+            continue
+
+        zh_galleries: list[str] = pattern.findall(zh_page.text)
+        en_galleries: list[str] = pattern.findall(en_text)
+        if len(en_galleries) != len(zh_galleries):
+            logging.info(f"gallery count mismatch for {zh_page.title()}")
+            continue
+
+        it = iter(en_galleries)
+        zh_page.text, cnt = pattern.subn(lambda _: next(it), zh_page.text)
+
+        if not cnt:
+            logging.debug(f"same galleries for {zh_page.title()}")
+            continue
+
+        zh_page.save(summary=f"Sync galleries with en. {cnt} replacements.")
+
+
+if __name__ == "__main__":
+    sync_galleries()
