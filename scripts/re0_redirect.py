44f7abb7e440ebbedfcc054e2578615c1a13087e
CCXXXI <ccxxxi@qq.com>
2023-04-15 06:44:24 +0800

    perf(redirect): includeredirects=False
    似乎没有建立多重重定向的需求

--- scripts/re0_redirect.py
+++ scripts/re0_redirect.py
@@ -8,7 +8,7 @@ pattern = re.compile(r".+?:(.+)")
 
 def create_redirect():
     """Create redirect [[stem]] for given [[prefix:stem]]."""
-    for target in tqdm(AllpagesPageGenerator()):
+    for target in tqdm(AllpagesPageGenerator(includeredirects=False)):
         if match := pattern.fullmatch(target.title()):
             if not (page := Page(target.site, match.group(1))).isRedirectPage():
                 page.set_redirect_target(
