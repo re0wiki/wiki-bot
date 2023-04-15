1238738dbef56929eea2f14ef8486bc1b3b48e1b
Restyled.io <commits@restyled.io>
2023-04-15 23:27:20 +0000

    style: restyle by black

--- rename.py
+++ rename.py
@@ -36,33 +36,39 @@ def rename(old, new):
     """Move pages and replace text."""
     o_pages = []
     for ns in ns_base + ["file"]:
-        pages = (CmdJob(
-            ["listpages", "-format:3", f"-titleregex:{old}",
-             ns2start(ns)]).run(simulate=True,
-                                capture_output=True).split("\n"))
+        pages = (
+            CmdJob(["listpages", "-format:3", f"-titleregex:{old}", ns2start(ns)])
+            .run(simulate=True, capture_output=True)
+            .split("\n")
+        )
         for page in pages:
             if page:
                 o_pages.append(ns + ":" + page)
 
     for o_page in o_pages:
         n_page = re.sub(old, new, o_page, flags=re.I)
-        CmdJob([
-            "movepages",
-            "-always",
-            f"-from:{o_page}",
-            f"-to:{n_page}",
-        ]).run()
+        CmdJob(
+            [
+                "movepages",
+                "-always",
+                f"-from:{o_page}",
+                f"-to:{n_page}",
+            ]
+        ).run()
 
-    CmdJob([
-        "replace",
-        "-automaticsummary",
-        "-always",
-        "-nocase",
-        "-regex",
-        r"-exceptinside:\[\[:?(zh|de|en|es|fr|it|nl|pl|pt-br|ru|uk|wp|wikipedia)\s?:[^\]]*\]\]",
-        old,
-        new,
-    ] + starts_more).run()
+    CmdJob(
+        [
+            "replace",
+            "-automaticsummary",
+            "-always",
+            "-nocase",
+            "-regex",
+            r"-exceptinside:\[\[:?(zh|de|en|es|fr|it|nl|pl|pt-br|ru|uk|wp|wikipedia)\s?:[^\]]*\]\]",
+            old,
+            new,
+        ]
+        + starts_more
+    ).run()
 
 
 if __name__ == "__main__":
--- scripts/re0_redirect.py
+++ scripts/re0_redirect.py
@@ -10,12 +10,10 @@ def create_redirect():
     """Create redirect [[stem]] for given [[prefix:stem]]."""
     for target in tqdm(AllpagesPageGenerator(includeredirects=False)):
         if match := pattern.fullmatch(target.title()):
-            if not (page := Page(target.site,
-                                 match.group(1))).isRedirectPage():
+            if not (page := Page(target.site, match.group(1))).isRedirectPage():
                 page.set_redirect_target(
-                    target,
-                    force=True,
-                    summary=f"{page.title()} -> {target.title()}")
+                    target, force=True, summary=f"{page.title()} -> {target.title()}"
+                )
 
 
 if __name__ == "__main__":
