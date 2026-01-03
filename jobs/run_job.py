a2726c491d5ef24943cd6e010ca0f44360810270
CCXXXI <ccxxxi@qq.com>
2026-01-03 19:39:04 +0800

    refactor: auto add "-always" or "-simulate"

--- jobs/run_job.py
+++ jobs/run_job.py
@@ -3,19 +3,15 @@ from subprocess import CalledProcessError, run
 
 
 def run_job(job: list[str], simulate=False, capture_output=False) -> str:
-    # Preprocess the job.
-    if simulate:
-        if "-always" in job:
-            job.remove("-always")
-        job.append("-simulate")
-    job[0:0] = ["python", "pywikibot/pwb.py"]
+    # Get the command line.
+    cmd = ["python", "pywikibot/pwb.py", *job, "-simulate" if simulate else "-always"]
 
     # Run the job.
     logging.info("=" * 16 + "start" + "=" * 16)
-    logging.info(job)
+    logging.info(cmd)
     try:
         res = run(
-            job,
+            cmd,
             capture_output=capture_output,
             encoding="utf8",
             check=True,
@@ -24,6 +20,6 @@ def run_job(job: list[str], simulate=False, capture_output=False) -> str:
         logging.error(e)
         return ""
     finally:
-        logging.info(job)
+        logging.info(cmd)
         logging.info("=" * 16 + "end" + "=" * 16)
     return res.stdout
