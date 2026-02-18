bbd45f26db394bb67fed891b6d3a095257c17e62
CCXXXI <ccxxxi@qq.com>
2026-02-18 19:45:55 +0800

    fix: re-implement re0_image.py

--- scripts/re0_image.py
+++ scripts/re0_image.py
@@ -1,74 +1,85 @@
-import logging
 from os import path
 from tempfile import TemporaryDirectory
 
 from tqdm import tqdm
 
-import pywikibot
-from pywikibot import exceptions
+import pywikibot as pwb
 
 
-def transfer_file(
-    *,
-    target_site: pywikibot.Site,
-    source_site: pywikibot.Site,
-    source_page: pywikibot.FilePage,
-):
-    """搬运一张图片。"""
-    if not target_site.logged_in():
-        target_site.login()
+def all_images(code: str) -> dict[str, pwb.FilePage]:
+    """返回所有图片的从页面名到页面的映射。"""
+    site = pwb.Site(code, "re0")
+    site.login()
+    return {
+        image.title(): image
+        for image in tqdm(site.allimages(), f"Collecting {code} images")
+    }
 
-    title = source_page.title(as_filename=True, with_ns=False)
-    if title.startswith("Site-"):
-        return
-    target_page: pywikibot.FilePage = pywikibot.FilePage(target_site, title)
 
-    if source_page is None:
-        logging.warning("source_page is None. title=%s", title)
-        return
+def calc_diff(
+    en_images: dict[str, pwb.FilePage], zh_images: dict[str, pwb.FilePage]
+) -> list[pwb.FilePage]:
+    """返回 en 的图片中，zh 缺失或过时的部分。"""
+    return [
+        image
+        for title, image in en_images.items()
+        if (
+            title not in zh_images
+            or zh_images[title].latest_file_info.timestamp
+            < image.latest_file_info.timestamp
+        )
+    ]
 
-    if (
-        target_page.exists()
-        and target_page.latest_file_info.sha1 == source_page.latest_file_info.sha1
-    ):
-        return
 
-    text = "\n".join([x.astext() for x in source_page.iterlanglinks()])
-    if text != "":
-        text += "\n"
-    text += f"[[{source_site.code}:{source_page.title(with_ns=True)}]]"
+def download_one(image: pwb.FilePage, tmp_dir: str) -> None:
+    """从 en 下载一张图片文件到临时目录。"""
+    filename = path.join(tmp_dir, image.title(with_ns=False, as_filename=True))
+    try:
+        image.download(filename)
+    except Exception as e:
+        pwb.logging.error(e)
 
-    logging.info("upload %s", title)
-    with TemporaryDirectory() as tmp_dir:
-        filename = path.join(tmp_dir, title)
-        if not source_page.download(filename):
-            logging.warning("download failed. title=%s", title)
-            return
-        target_page.upload(
+
+def upload_one(image: pwb.FilePage, tmp_dir: str) -> None:
+    """从临时目录上传一张图片文件到 zh。"""
+    filename = path.join(tmp_dir, image.title(with_ns=False, as_filename=True))
+    title = image.title()
+    text = f"[[en:{title}]]"
+
+    try:
+        pwb.FilePage(pwb.Site("zh", "re0"), title).upload(
             filename,
             comment=text,
             text=text,
             report_success=False,
             ignore_warnings=True,
         )
+    except Exception as e:
+        pwb.logging.error(e)
 
 
-def transfer(*, source, target):
-    """搬运图片。"""
-    logging.info("source=%s, target=%s", source, target)
+def download_all(images: list[pwb.FilePage], tmp_dir: str):
+    """从 en 下载所有图片文件到临时目录。"""
+    pwb.Site("en", "re0").login()
+    for image in tqdm(images, "Downloading images"):
+        download_one(image, tmp_dir)
 
-    for page in tqdm(list(source.allimages())):
-        assert isinstance(page, pywikibot.FilePage)
-        try:
-            transfer_file(target_site=target, source_site=source, source_page=page)
-        except (exceptions.PageRelatedError, exceptions.APIError, ValueError) as e:
-            logging.warning(e)
-            continue
+
+def upload_all(images: list[pwb.FilePage], tmp_dir: str):
+    """从临时目录上传所有图片文件到 zh。"""
+    pwb.Site("zh", "re0").login()
+    for image in tqdm(images, "Uploading images"):
+        upload_one(image, tmp_dir)
+
+
+def main() -> None:
+    en_images = all_images("en")
+    zh_images = all_images("zh")
+    diff = calc_diff(en_images, zh_images)
+    with TemporaryDirectory() as tmp_dir:
+        download_all(diff, tmp_dir)
+        upload_all(diff, tmp_dir)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    )
-    transfer(source=pywikibot.Site("en", "re0"), target=pywikibot.Site("zh", "re0"))
+    main()
