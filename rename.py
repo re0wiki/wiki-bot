e16cb936c9a24be7a3c025209ad0632f20d87e9e
CCXXXI <ccxxxi@qq.com>
2026-04-04 17:57:04 +0800

    fix(rename): correct and simplify

--- rename.py
+++ rename.py
@@ -1,63 +1,52 @@
 import argparse
 import logging
-import re
 
 from jobs.run_job import run_job
 from jobs.starts import ns2start, ns_base, starts_more
 
-# region logging
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
-# endregion
-
-# region parser
-parser = argparse.ArgumentParser(description="移动页面 & 替换文本")
-parser.add_argument("old")
-parser.add_argument("new")
-# endregion
 
 
 def rename(old, new):
     """Move pages and replace text."""
-    o_pages = []
+    o_pages: list[str] = []
     for ns in ns_base + ["file"]:
         pages = run_job(
             ["listpages", "-format:3", f"-titleregex:{old}", ns2start(ns)],
             simulate=True,
             capture_output=True,
-        ).split("\n")
-        for page in pages:
+        )
+        if not pages:
+            continue
+        for page in pages.split("\n"):
             if page:
                 o_pages.append(ns + ":" + page)
 
+    move_cmd = ["python", "pywikibot/pwb.py", "movepages"]
     for o_page in o_pages:
-        n_page = re.sub(old, new, o_page, flags=re.I)
-        run_job(
-            [
-                "movepages",
-                "-always",
-                f"-from:{o_page}",
-                f"-to:{n_page}",
-            ]
-        )
+        n_page = o_page.replace(old, new)
+        move_cmd += [f"-from:'{o_page}'", f"-to:'{n_page}'"]
+    print(" ".join(move_cmd))
 
-    run_job(
-        [
-            "replace",
-            "-automaticsummary",
-            "-always",
-            "-nocase",
-            "-regex",
-            r"-exceptinside:\[\[:?(zh|de|en|es|fr|it|nl|pl|pt-br|ru|uk|wp|wikipedia)\s?:[^\]]*\]\]",
-            old,
-            new,
-        ]
-        + starts_more
-    )
+    replace_cmd = [
+        "python",
+        "pywikibot/pwb.py",
+        "replace",
+        "-automaticsummary",
+        r"-exceptinside:'\[\[:?(zh|de|en|es|fr|it|nl|pl|pt-br|ru|uk|wp|wikipedia)\s?:[^\]]*\]\]'",
+        f"'{old}'",
+        f"'{new}'",
+    ] + starts_more
+    print(" ".join(replace_cmd))
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="移动页面 & 替换文本")
+    parser.add_argument("old")
+    parser.add_argument("new")
+
     args = parser.parse_args()
     rename(args.old, args.new)
